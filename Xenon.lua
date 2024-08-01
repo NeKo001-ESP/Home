b1660875d341f21967f1b6d7538db7a756c32f59
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-02 01:44:26 +0700

    Update Xenon.lua

--- Xenon.lua
+++ Xenon.lua
@@ -1,13 +1,2 @@
-getgenv().Configuration = {
-  ['Enabled'] = true,
-  ['Enabled Challenge'] = false,
-  ['PC Name'] = 'DDC-1',
-  ['Delay'] = 0,
-  ['Leave Delay'] = 0,
-  ['EquipBest'] = true,
-  ['Leave At Wave'] = 31,
-  ['Leave Method'] = 1, -- 1 = Sell,  2 = Leave
-  ['TradingMode'] = false,
-};
 getgenv().key = '205534130058'
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a9be77bec1dd9acd72e2b8289f71adf7.lua"))()
