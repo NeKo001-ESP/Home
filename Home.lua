cf38e0303d7860e5c48bc4f4b08b2329d27bd09a
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 15:25:21 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,13 +1,3 @@
-local PlaceId = game.PlaceId
-local GameId = game.gameId
-
-if (PlaceId == 17764698696) or (PlaceId == 17017769292) or (PlaceId == 17018663967) or (PlaceId == 3351674303) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon.lua"))()
-end
-if (PlaceId == 2753915549) or (PlaceId == 4442272183) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Royx.lua"))()
-end
-
 getgenv().AD = {
     Load = {
         ["Anime"] = "true",
@@ -17,4 +7,14 @@ getgenv().AD = {
 if getgenv().AD.Load.Anime == "true" then
     local scriptContent = game:HttpGet("")
     loadstring(scriptContent)()
+end
+
+local PlaceId = game.PlaceId
+local GameId = game.gameId
+
+if (PlaceId == 17764698696) or (PlaceId == 17017769292) or (PlaceId == 17018663967) or (PlaceId == 3351674303) then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon.lua"))()
+end
+if (PlaceId == 2753915549) or (PlaceId == 4442272183) then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Royx.lua"))()
 end
