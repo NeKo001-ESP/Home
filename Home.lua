3bfd09e659b1263ff571533476e3d6f3247e1a53
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 10:57:25 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -5,3 +5,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main
 local gameid = game.PlaceId
 if(gameid == 17017769292) then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
+end
