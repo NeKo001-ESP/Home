32dcbe8efef13952db5087d06f75fa10c6214b33
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 12:09:39 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,7 +1,9 @@
-local PlaceId = game.PlaceId  -- Gets the current game's place ID
-local GameId = game.gameId    -- Gets the current game's ID (might be different)
+local PlaceId = game.PlaceId
+local GameId = game.gameId
 
--- Check for specific place IDs
 if (PlaceId == 17764698696) or (PlaceId == 17017769292) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()  -- Attempts to download and run "Xenon_AD.lua"
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
 end
+if (GameId == 3351674303) then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon.lua"))()
+end  
