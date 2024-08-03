6584e9fa5fc5482d8d3116aa0bd619130ab25899
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 11:08:22 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -5,6 +5,4 @@ if GameId == 17764698696 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
 elseif GameId == 17017769292 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
-else
-    print("GameId does not match any known ID.")
 end
