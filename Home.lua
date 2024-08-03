798918785e4fcbc0071a3b64c1402140aa759e6e
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 11:29:41 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -3,10 +3,6 @@ local GameId = game.gameId
 
 If game place id = 17764698696
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
-else
-print("wrong game")
 If game place id = 17017769292
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
-else
-print("wrong game")
 end
