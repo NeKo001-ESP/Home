9a6a3a506b40ca86d63a433c60a4efe4470379da
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 11:53:31 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -5,6 +5,6 @@ if (PlaceId == 17764698696) or (PlaceId == 17017769292) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
 end
 if (PlaceId == 3351674303) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/blob/main/Xenon.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon.lua"))()
 end
 
