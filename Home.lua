215a57a0716d30958da6aeefc96eaefed7f96780
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 10:54:05 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,7 +1,6 @@
 local gameid = game.PlaceId
 if(gameid == 17764698696) then
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua")() --Anime Defenders_New_Player
-else
-  if(gameid == 17017769292) then
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua")() --Anime Defenders
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
+local gameid = game.PlaceId
+if(gameid == 17017769292) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
