719f5a8cbf3fc2d290f01a5f7384f8ce40356384
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 10:32:01 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,7 +1,4 @@
 local GameId = game.GameId
-
---// Tables
-
 local Games = {
     { name = "Anime-Defenders-New-Players", gameid = 17764698696, link = "https://raw.githubusercontent.com/NeKo001-ESP/Home/blob/main/Xenon_AD.lua" },
     { name = "Anime-Defenders", gameid = 17017769292, link = "https://raw.githubusercontent.com/NeKo001-ESP/Home/blob/main/Xenon_AD.lua" },
