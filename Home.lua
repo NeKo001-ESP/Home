ae476aca6dcd526417d3a9b859a589cfd09126bd
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 15:22:13 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -6,7 +6,7 @@ if (PlaceId == 17764698696) or (PlaceId == 17017769292) or (PlaceId == 170186639
 end
 if (PlaceId == 2753915549) or (PlaceId == 4442272183) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Royx.lua"))()
-end
+
 
 
 
