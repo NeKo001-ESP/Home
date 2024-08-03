f0955390ae583b8c6cf86f62414d6b10e26a7653
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 11:43:28 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,8 +1,8 @@
 local PlaceId = game.PlaceId
 local GameId = game.gameId
 
-elseif game place id = 17764698696
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
-elseif game place id = 17017769292
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
+-- Check if the current game's place ID matches specific values
+if (PlaceId == 17764698696) or (PlaceId == 17017769292) then
+  -- If it matches, attempt to load and execute external code
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
 end
