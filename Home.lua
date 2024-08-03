1047ac91633ebdeda92a8079b71efa18edc37427
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 11:44:07 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,8 +1,6 @@
 local PlaceId = game.PlaceId
 local GameId = game.gameId
 
--- Check if the current game's place ID matches specific values
 if (PlaceId == 17764698696) or (PlaceId == 17017769292) then
-  -- If it matches, attempt to load and execute external code
   loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
 end
