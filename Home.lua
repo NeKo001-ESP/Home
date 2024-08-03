e99882828c7fee217130078ec7b27a00cbdcba9b
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 15:21:39 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -7,6 +7,9 @@ end
 if (PlaceId == 2753915549) or (PlaceId == 4442272183) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Royx.lua"))()
 end
+
+
+
 getgenv().AD = {
   Load = {
      ["Anime"] = "", 
