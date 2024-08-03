864c6ceb2156cbe09bb0cb0e8c26134166ec3ac9
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 11:20:28 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,7 +1,7 @@
 local gameid = game.PlaceId
 if(gameid == 17017769292) then
 print("place valid")
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
 else
 print("place invalid")
 end
