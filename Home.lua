f315c062f69294dacdf2f5c6708eaea2c639e3b8
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-03 12:38:18 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,8 +1,8 @@
-local Pid = game.PlaceId
-local Gid = game.gameId
+local PlaceId = game.PlaceId
+local GameId = game.gameId
 
-if (Gid == 17764698696) or (Gid == 17017769292) then
+elseif (GameId == 17764698696) or (Gid == 17017769292) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
-elseif (Gid == 3351674303) then
+elseif (GameId == 3351674303) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon.lua"))()
 end
