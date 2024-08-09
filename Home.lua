fcba825e0c62e17a4fce275db4d7ef0325e65c15
NeKo001-ESP <tcamikr6@gmail.com>
2024-08-09 23:05:18 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
 
 --PH        https://raw.githubusercontent.com/NeKo001-ESP/Home/main/PH.lua
 --Royx      https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Royx.lua
