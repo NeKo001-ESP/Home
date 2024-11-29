3bfe7daab187846fa771b013f369efe34d489248
NeKo001-ESP <tcamikr6@gmail.com>
2024-11-29 08:37:17 +0700

    Update Xenon_AD.lua

--- Xenon_AD.lua
+++ Xenon_AD.lua
@@ -1,13 +1,26 @@
 getgenv().Configuration = {
-  ['Enabled'] = true,
-  ['Enabled Challenge'] = true,
-  ['PC Name'] = 'DDC-1',
-  ['Delay'] = 0,
-  ['Leave Delay'] = 0,
-  ['EquipBest'] = true,
-  ['Leave At Wave'] = 31,
-  ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
-  ['TradingMode'] = false,
-};
-getgenv().key = '205534130058'
+    Enabled = true,
+    ZoneFish = 'The Depths', -- The Depths // Forsaken Pond // Snowcap // Vertigo
+    EquipRod = 'Auto', --  Auto // Aurora Rod // Trident Rod // Kings Rod // Sunken Rod // Rapid Rod // etc.
+    Rods = {
+        ['Trident Rod'] = false,
+        ['Aurora Rod'] = true,
+        ['Kings Rod'] = false,
+        ['Sunken Rod'] = false,
+        ['Mythical Rod'] = false, 
+        ['Destiny Rod'] = false,
+        ['Rod Of The Depths'] = true,
+    },
+    AutoSell = {
+        ['Enabled'] = true,
+        ['Sell every'] = 200, -- seconds
+        ['Mythical'] = false,
+        ['Legendary'] = true,
+        ['Enchant Rellcs'] = false,
+        ['Exotic'] = true,
+        ['Event Fish'] = true,
+        ['Limited Fish'] = true,
+    },
+}
+getgenv().key = '5d3373fe-c8b1-4e08-baaf-9ea38de4287f'
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
