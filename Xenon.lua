41457ece61c179dc31cda4d4157b031889f0995e
NeKo001-ESP <tcamikr6@gmail.com>
2024-11-29 08:32:03 +0700

    Update Xenon.lua

--- Xenon.lua
+++ Xenon.lua
@@ -1,2 +1,2 @@
-getgenv().key = '205534130058'
+getgenv().key = '5d3373fe-c8b1-4e08-baaf-9ea38de4287f'
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
