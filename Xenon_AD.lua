9992535a1058518bd6c9ccb6f5a544f15dca5a40
NeKo001-ESP <tcamikr6@gmail.com>
2024-11-30 00:03:52 +0700

    Update Xenon_AD.lua

--- Xenon_AD.lua
+++ Xenon_AD.lua
@@ -3,12 +3,12 @@ getgenv().Configuration = {
     ZoneFish = 'The Depths', -- The Depths // Forsaken Pond // Snowcap // Vertigo
     EquipRod = 'Rapid Rod', --  Auto // Aurora Rod // Trident Rod // Kings Rod // Sunken Rod // Rapid Rod // etc.
     Rods = {
-        ['Trident Rod'] = false,
+        ['Trident Rod'] = true,
         ['Aurora Rod'] = true,
-        ['Kings Rod'] = false,
-        ['Sunken Rod'] = false,
-        ['Mythical Rod'] = false, 
-        ['Destiny Rod'] = false,
+        ['Kings Rod'] = true,
+        ['Sunken Rod'] = true,
+        ['Mythical Rod'] = true, 
+        ['Destiny Rod'] = true,
         ['Rod Of The Depths'] = true,
     },
     AutoSell = {
