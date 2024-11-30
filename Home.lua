1bafb34a552aa3d563f7ba221e8283687155bf6f
NeKo001-ESP <tcamikr6@gmail.com>
2024-11-30 09:55:06 +0700

    Update Home.lua

--- Home.lua
+++ Home.lua
@@ -1,4 +1,3 @@
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon.lua"))()
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Xenon_AD.lua"))()
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/NeKo001-ESP/Home/main/Royx.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/rivals.lua"))()
