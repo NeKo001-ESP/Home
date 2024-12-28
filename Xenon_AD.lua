58eda8b15016ec834db6a7eceb4eb31cf0f06c65
NeKo001-ESP <tcamikr6@gmail.com>
2024-12-28 08:30:07 +0700

    Update Xenon_AD.lua

--- Xenon_AD.lua
+++ Xenon_AD.lua
@@ -23,4 +23,4 @@ getgenv().Configuration = {
     },
 }
 getgenv().key = '5d3373fe-c8b1-4e08-baaf-9ea38de4287f'
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))();
